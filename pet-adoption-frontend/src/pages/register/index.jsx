3982f1a53a857c9f734c3ca22c4a5d5f6f56a2a3
Jan Jasa <jan_jasa1@baylor.edu>
2024-09-24 23:54:58 -0500

    Adding register page template

--- pet-adoption-frontend/src/pages/register/index.jsx
+++ pet-adoption-frontend/src/pages/register/index.jsx
@@ -3,14 +3,60 @@ import React from "react";
 
 
 export default function register() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add the functionality of signing in
+        console.log("Form Submitted")
+    }
+
     return (
         <>
             <Head>
-                <title>Register | Baylor Furries</title>
+                <title>Register | Furever Homes</title>
             </Head>
 
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'Center', height: '50vh'}}>
+                <div style={{textAlign: 'center'}}>
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', width: '300px', margin: '0 auto'}}>
+                        <input
+                            type="name"
+                            placeholder="Name"
+                            style={{padding: '10px', margin: '10px 0', borderRadius: '5px', border: '1px solid #ccc'}}
+                            required
+                        />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            style={{padding: '10px', margin: '10px 0', borderRadius: '5px', border: '1px solid #ccc'}}
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            style={{padding: '10px', margin: '10px 0', borderRadius: '5px', border: '1px solid #ccc'}}
+                            required
+                        />
 
-            <h1 style={{marginLeft: '1100px', marginTop: '250px'}}>REGISTER</h1>
+                        <button
+
+                            type={"submit"}
+                            style={{
+                                padding: '10px',
+                                margin: '10px 0',
+                                backgroundColor: 'cornflowerblue',
+                                color: 'white',
+                                border: 'none',
+                                borderRadius: '5px',
+                                cursor: 'pointer'
+                            }}
+                            >
+
+                            Register
+                        </button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
