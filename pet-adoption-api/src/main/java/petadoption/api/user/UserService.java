ddd77e69bf74d0a7f667b97989e63afa593b0e6d
Calrus <devan_sengel1@baylor.edu>
2024-09-25 10:38:23 -0500

    added user setters

--- pet-adoption-api/src/main/java/petadoption/api/user/UserService.java
+++ pet-adoption-api/src/main/java/petadoption/api/user/UserService.java
@@ -17,4 +17,17 @@ public class UserService {
     public User saveUser(User user) {
         return userRepository.save(user);
     }
+
+    public User registerUser(String username, String password, String email, String userType) {
+        if(userRepository.findByEmail(email).isPresent()) {
+            throw new IllegalStateException("Email already exists");
+        }
+        // Currently plaintext until we decide on encryption method.
+        User user = new User();
+        user.setEmailAddress(email);
+        user.setPassword(password);
+        user.setUserType(userType);
+
+        return userRepository.save(user);
+    }
 }
