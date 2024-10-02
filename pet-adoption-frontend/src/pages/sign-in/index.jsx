6ace42e353e89140e656e4ccd6bcc32bd5c85127
Mark Josephs <95541571+mjosephs04@users.noreply.github.com>
2024-10-01 22:49:20 -0500

    fixing build

--- pet-adoption-frontend/src/pages/sign-in/index.jsx
+++ pet-adoption-frontend/src/pages/sign-in/index.jsx
@@ -36,7 +36,7 @@ export default function signIn() {
                 const data = await response.json()
                 console.log(data)
 
-                Cookies.set('authToken', data.authToken, { expires: 7, secure: false, path: '/', domain: ${process.env.NEXT_PUBLIC_API_URL} });
+                Cookies.set('authToken', data.authToken);
                 await router.push('/account');
             }
         } catch (error) {
