66032a24f0e3fcbf263cfdc15cda839f518d70d3
markjosephs <mjosephs@customsportssleeves.com>
2024-10-08 23:43:28 -0500

    adding populate functionality to user-details

--- pet-adoption-frontend/src/pages/account-details/index.jsx
+++ pet-adoption-frontend/src/pages/account-details/index.jsx
@@ -1,6 +1,6 @@
 import {useRouter} from "next/router";
 import {Button, Paper, TextField, Typography} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Cookies from "js-cookie";
 export default function userAcct() {
     const router = useRouter();
@@ -8,6 +8,35 @@ export default function userAcct() {
     const [emailAddress, setEmailAddress] = useState('');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
+
+    useEffect(() => {
+        const getUserDetails = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}:8080/api/auth/getUser`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': Cookies.get('authToken')
+                    },
+                });
+
+                if (!response.ok) {
+                    console.error('Authentication failed', response.statusText);
+                }
+
+                const data = await response.json();
+
+                // Assuming the response contains the fields emailAddress, firstName, and lastName
+                setEmailAddress(data.emailAddress);
+                setFirstName(data.firstName);
+                setLastName(data.lastName);
+
+            } catch (error) {
+                console.error("Error during checkAuth", error);
+            }
+        };
+        getUserDetails()
+    }, []);
 
     const handleSubmit = async () => {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}:8080/api/auth/update`, {
@@ -53,9 +82,9 @@ export default function userAcct() {
                 gap: '16px'
             }} elevation={4}>
                 <Typography variant="h5" sx={{marginBottom: '35px', fontWeight: 'bold'}}>Edit Account Details</Typography>
-                <TextField id="outlined-basic" label="First Name" variant="outlined" sx={{width: '50%'}} onChange={(event) => setFirstName(event.target.value)} />
-                <TextField id="outlined-basic" label="Last Name" variant="outlined" sx={{width: '50%'}} onChange={(event) => setLastName(event.target.value)} />
-                <TextField id="outlined-basic" label="Email" variant="outlined" sx={{width: '50%'}} onChange={(event) => setEmailAddress(event.target.value)} />
+                <TextField id="outlined-basic" label="First Name" variant="outlined" sx={{width: '50%'}} onChange={(event) => setFirstName(event.target.value)} value={firstName} />
+                <TextField id="outlined-basic" label="Last Name" variant="outlined" sx={{width: '50%'}} onChange={(event) => setLastName(event.target.value)} value={lastName} />
+                <TextField id="outlined-basic" label="Email" variant="outlined" sx={{width: '50%'}} onChange={(event) => setEmailAddress(event.target.value)} value={emailAddress} />
 
                 <Button variant="outlined"
                         size="large"
