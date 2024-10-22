68e2e754a18193ef70c945d9cbfba84d3a54a749
markjosephs <mjosephs@customsportssleeves.com>
2024-10-22 12:40:54 -0500

    adding contact us functionality, cleaning up pages

--- pet-adoption-frontend/src/pages/not-authorized/index.jsx
+++ pet-adoption-frontend/src/pages/not-authorized/index.jsx
@@ -1,8 +1,26 @@
-import {Typography} from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
 
-
-export default function noAuth(){
-    return(
-        <Typography> not authorized </Typography>
-    )
+export default function NoAuth() {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80vh",
+                textAlign: "center",
+                gap: 2,
+            }}
+        >
+            <LockIcon sx={{ fontSize: 80, color: "#757575" }} />
+            <Typography variant="h4" component="h1">
+                Not Authorized
+            </Typography>
+            <Typography variant="body1">
+                You don't have permission to access this page.
+            </Typography>
+        </Box>
+    );
 }
