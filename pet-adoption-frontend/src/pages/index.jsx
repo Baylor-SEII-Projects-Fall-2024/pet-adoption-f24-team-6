a5709f220b39a030b21a70dbee83d39ceb97b06a
markjosephs <mjosephs@customsportssleeves.com>
2024-10-23 17:28:22 -0500

    commit for demo

--- pet-adoption-frontend/src/pages/index.jsx
+++ pet-adoption-frontend/src/pages/index.jsx
@@ -7,15 +7,16 @@ import styles from '@/styles/Home.module.css'
 export default function HomePage() {
 
   return (
-    <>
-      <Head>
-        <title>Home Page | Baylor Furries</title>
-      </Head>
+      <>
+          <Head>
+              <title>Home Page | Baylor Furries</title>
+          </Head>
 
-        <Box sx={{height: "20px"}}></Box>
-        <ImageCarousel />
-      <h1 style={{marginLeft: '1100px', marginTop: '250px'}}>HOMEPAGE</h1>
+          <Box sx={{height: "20px"}}></Box>
+          <ImageCarousel/>
+          <h1 style={{marginLeft: '1100px', marginTop: '250px'}}>HOMEPAGE</h1>
+          <h1 style={{marginLeft: '1100px', marginTop: '250px'}}>Demo for CICD</h1>
 
-    </>
+      </>
   );
 }
