2c8b69392a800d99be7f305af376ed2373889e42
markjosephs <mjosephs@customsportssleeves.com>
2024-11-06 13:49:22 -0600

    fixing small bug on pet page

--- pet-adoption-frontend/src/pages/pet/[petID]/index.js
+++ pet-adoption-frontend/src/pages/pet/[petID]/index.js
@@ -47,8 +47,6 @@ export default function PetDetails() {
                         return;
                     }
                     setUserID(data.userID);
-                } else {
-                    router.push('/not-authorized');
                 }
             } catch (error) {
                 console.error("Error fetching user type:", error);
