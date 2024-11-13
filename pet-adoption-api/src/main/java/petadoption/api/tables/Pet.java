812d9e95452f12a0777f9d45ce0709c4345f940f
Calrus <devan_sengel1@baylor.edu>
2024-11-13 14:40:56 -0600

    Set to 0 maybe?

--- pet-adoption-api/src/main/java/petadoption/api/tables/Pet.java
+++ pet-adoption-api/src/main/java/petadoption/api/tables/Pet.java
@@ -48,13 +48,13 @@ public class Pet {
     Integer trainingLevel;
 
     @Column(name = "LIKES")
-    Integer likes;
+    Integer likes = 0;
 
     @Column(name = "DISLIKES")
-    Integer dislikes;
+    Integer dislikes = 0;
 
     @Column(name = "VIEWS")
-    Integer views;
+    Integer views = 0;
 
     @ManyToOne
     @JoinColumn(name = "CENTER_ID", referencedColumnName = "CENTER_ID")
